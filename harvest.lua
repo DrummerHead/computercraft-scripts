a68931b74045720ce45f5a362f0a68a6c3042889
DrummerHead <drummerhead@gmail.com>
2015-01-26 21:38:11 -0200

    Improve harvesting script, change shape, add fields parameter

--- harvest.lua
+++ harvest.lua
@@ -2,17 +2,21 @@
 -- Put seeds on slot 1
 --
 -- Description:
--- Takes care of harvesting and seeding, not a very modular script
--- Pattern expected as such:
+-- Takes care of harvesting and seeding
+-- It works with 2 by {{length}} structures separated by 1 water
 --
--- XXXXXXXX  \
+-- XX XX XX  \
 -- XX XX XX  |
 -- XX XX XX  |
--- XX XX XX  | Height as only parameter
+-- XX XX XX  | Height (first parameter)
 -- XX XX XX  |
 -- XX XX XX  |
--- XXXXXXXX  /
+-- XX XX XX  /
+-- \______/
+--
+--   Fields (second parameter)
 -- 
+--
 -- The Xs will be harvested and replanted
 
 
@@ -24,64 +28,42 @@ function harvest_line(length)
   end
 end
 
-function turn(is_left)
-  if is_left then
-    turtle.digDown()
-    turtle.placeDown()
-    turtle.turnLeft()
-    turtle.forward()
-    turtle.turnLeft()
-  else
-    turtle.digDown()
-    turtle.placeDown()
-    turtle.turnRight()
-    turtle.forward()
-    turtle.turnRight()
-  end
+function turn_left()
+  turtle.digDown()
+  turtle.placeDown()
+  turtle.turnLeft()
+  turtle.forward()
+  turtle.turnLeft()
 end
 
 function pass_water()
+  turtle.digDown()
+  turtle.placeDown()
   turtle.turnRight()
-  harvest_line(3)
+  turtle.forward()
+  turtle.forward()
   turtle.turnRight()
 end
 
-function harvest(length)
-  harvest_line(length)
-  turn(true)
-  harvest_line(length)
-  pass_water()
-  harvest_line(length)
-  turn(true)
-  harvest_line(length)
-  pass_water()
-  harvest_line(length)
-  turn(true)
-  harvest_line(length)
-  
-  -- awkward...
+function harvest(length, fields)
+  for i = 1, fields do
+    harvest_line(length)
+    turn_left()
+    harvest_line(length)
+    if i ~= fields then
+      pass_water()
+    end
+  end
+
   turtle.digDown()
   turtle.placeDown()
   turtle.turnLeft()
-  turtle.turnLeft()
 
-  for i = 2, length do
+  for j = 0, ((fields -1) * 3) do
     turtle.forward()
   end
 
-  turtle.turnRight()
-  turtle.forward()
-  turtle.forward()
-
-  turtle.digDown()
-  turtle.placeDown()
-
-  turtle.forward()
-  turtle.forward()
-  turtle.forward()
-
-  turtle.digDown()
-  turtle.placeDown()
+  turtle.turnLeft()
 end
 
-harvest(20)
+harvest(20, 3)

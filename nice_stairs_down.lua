b36ef35659d229f8ea6f2301d4a189e5b33b6414
DrummerHead <drummerhead@gmail.com>
2015-01-29 23:55:51 -0200

    On nice_stairs_down detect lava and have safe forward and down (avoid falling blocks)

--- nice_stairs_down.lua
+++ nice_stairs_down.lua
@@ -5,16 +5,61 @@
 -- Description:
 -- Will build stairs going down
 -- and place torches every 6 diagonal
+-- Will avoid lava and water and return to the place 
+-- where initially triggered
 
 
-function stair(level)
+function safe_forward(steps)
+  for i = 1, steps do
+    while not turtle.forward() do
+      turtle.dig()
+    end
+  end
+end
+
+function safe_up(steps)
+  for i = 1, steps do
+    while not turtle.up() do
+      turtle.digUp()
+    end
+  end
+end
+
+function lava_or_water_down()
+  local inspectSuccess, inspectData = turtle.inspectDown()
+  if inspectSuccess then
+    return inspectData.name == 'minecraft:flowing_lava' or 
+    inspectData.name == 'minecraft:lava' or 
+    inspectData.name == 'minecraft:flowing_water' or
+    inspectData.name == 'minecraft:water'
+  else
+    return false
+  end
+end
+
+function go_back(level)
+  turtle.turnLeft()
+  turtle.turnLeft()
+  for i = 0, level do
+    safe_up(1)
+    safe_forward(1)
+  end
+end
+
+function stairs(level)
+  local y = 0
   for i = 1, level do
     turtle.dig()
+    if lava_or_water_down() then
+      go_back(y)
+      break
+    end
     turtle.digDown()
-    turtle.forward()
+    safe_forward(1)
     turtle.dig()
     turtle.digDown()
     turtle.down()
+    y = y + 1
     turtle.turnLeft()
     turtle.turnLeft()
     turtle.place()
@@ -28,14 +73,4 @@ function stair(level)
   end
 end
 
-function goBack(level)
-  for i = 1, level do
-    turtle.up()
-    turtle.back()
-  end
-end
-
-
-stair(12)
-
--- goBack(10)
+stairs(12)
